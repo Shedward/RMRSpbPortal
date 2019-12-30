901306a4469d89119c820effa377efe482a89f9b
Vlad Maltsev <v.maltsev@redmadrobot.com>
2019-12-30 18:51:11 +0300

    Added colors.

---
+++ RMRSpbPortal/Presentation/Common/Style/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  RMRSpbPortal
+//
+//  Created by Vladislav Maltsev on 02.11.2019.
+//  Copyright © 2019 RedMadRobot. All rights reserved.
+//
+
+import UIKit
+
+enum Style {
+    enum Color {
+        static let background = UIColor.required(named: "background")
+        static let content = UIColor.required(named: "content")
+        static let secondaryBackground = UIColor.required(named: "secondaryBackground")
+        static let secondaryContent = UIColor.required(named: "secondaryContent")
+    }
+}
